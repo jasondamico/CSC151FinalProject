858e85cc6a0485979479054af3435fc06cc2431d
Equipy <57501201+Equipy@users.noreply.github.com>
2022-03-09 14:16:34 -0500

    Update Main.java

--- assignment/Main.java
+++ assignment/Main.java
@@ -7,26 +7,54 @@ public class Main
     {
         Time day = new Time();
         int duration = 480; //8 hours
-        int population = 10; //editable - how many visitors the park is getting
 
         int fastPassUsage = 0; //for finding average fastpass usage
 
         Attraction att1 = new Attraction("The Stack Cyclone", 1, 5, 10);
         Attraction att2 = new Attraction("The Queue Coaster", 5, 8, 5);
         Attraction att3 = new Attraction("Linked List Log Ride", 3, 6, 8);
-        Attraction att4 = new Attraction("The Tree Tower", 4, 16, 3);
+        Attraction att4 = new Attraction("The Tree Tower", 4, 16, 5);
 
         Attraction[] listofRides = {att1, att2, att3, att4};
         Park codeLand = new Park("Code Land!", listofRides);
 
-        Person one = new Person(1, 120, duration, 60, true, false); //1 hour balk time
+        Person test = new Person();
+
 
         while (day.getCurrentTime() <= duration)
         {
+            //people enter park - list of people (random number)
+             /*
+                check each person in the list of entering people
+                ============================================
+                if (codeLand.pickAttraction() == null) {
+                        - add to people who left arrayList
+                }
+                else {
+                        codeLand.pickAttraction.enqueue(person being checked)
+                }
+                 */
+
+            for (Attraction ride: listofRides)
+            {
+                if (!ride.isCurrentlyRunning() && ride.getCurrentlyInLine() > 0)
+                {
+                    ride.startRide(); //puts people on the ride to capacity
+                    ride.setRideStartTime(day.getCurrentTime());
+                }
+                //pop all people off the ride
+                //pick attraction for people who could be leaving
+            }
+
+            //check if ride finished
+            //assign people getting off a new ride (pickAttraction)
+
+            //
             //use the list of people in the park, grab the people and have them pick their attraction
             //if they're already in a line and aren't boarding then they'll
-            //arrayofpeople who just got off ride, use pick attraction
-            
+            //arrayofpeople who just got off ride, use pick attraction (or leave)
+
+
 
             //arraylist of people who are in the park
 
