6eb40ddceda88f4a495cf6d14fb8c0e9884f2c20
Equipy <57501201+Equipy@users.noreply.github.com>
2022-03-10 12:53:22 -0500

    cleaned up a little & does stuff

--- assignment/Main.java
+++ assignment/Main.java
@@ -8,10 +8,22 @@ public class Main
     public static Time currentTime = new Time();
     public static int parkHours = 480; //8 hours
 
+    private int population;
+
+    public int getPopulation()
+    {
+        return population;
+    }
+
+    public void setPopulation(int population)
+    {
+        this.population = population;
+    }
+
     public static void main(String[] args)
     {
-
         int fastPassUsage = 0; //for finding average fastpass usage
+        int population = 10;
 
         Attraction att1 = new Attraction("The Stack Cyclone", 1, 5, 10);
         Attraction att2 = new Attraction("The Queue Coaster", 5, 8, 5);
@@ -26,23 +38,17 @@ public class Main
 
         Park codeLand = new Park("Code Land!", listofRides);
 
-        Person test = new Person();
+        ArrayList<Person> customers = new ArrayList<>();
+        for (int i = 0; i < population; i++)
+        {
+            Person guy = new Person(i);
+            customers.add(guy);
 
+            codeLand.pickAttraction(guy, codeLand.getAttractions());
+        }
 
         while (currentTime.getCurrentTime() <= parkHours)
         {
-            //people enter park - list of people (random number)
-             /*
-                check each person in the list of entering people
-                ============================================
-                if (codeLand.pickAttraction() == null) {
-                        - add to people who left arrayList
-                }
-                else {
-                        codeLand.pickAttraction.enqueue(person being checked)
-                }
-                 */
-
             for (Attraction ride: listofRides)
             {
                 if (!ride.isCurrentlyRunning() && ride.getCurrentlyInLine() > 0)
@@ -50,29 +56,13 @@ public class Main
                     ride.startRide(); //puts people on the ride to capacity
                     ride.setRideStartTime(currentTime.getCurrentTime());
                 }
-                //pop all people off the ride
-                //pick attraction for people who could be leaving
+                for (Person guy : ride.checkRuntime(currentTime.getCurrentTime()))
+                {
+                    codeLand.pickAttraction(guy, codeLand.getAttractions()); //removes people from park is this is null
+                }
             }
-
-            //check if ride finished
-            //assign people getting off a new ride (pickAttraction)
-
-            //
-            //use the list of people in the park, grab the people and have them pick their attraction
-            //if they're already in a line and aren't boarding then they'll
-            //arrayofpeople who just got off ride, use pick attraction (or leave)
-
-
-
-            //arraylist of people who are in the park
-
-            //wait times change/calculations
-            //go on rides
             currentTime.setCurrentTime(currentTime.getCurrentTime() + 5); //5 minutes pass
         }
-
-        //Population
-        //stepping
 
 
     }
