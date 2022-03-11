cb9e671ec08d4041a35bdc14599d41e51342945d
jasondamico <damicoj@union.edu>
2022-03-10 20:48:50 -0500

    Change min stay check
    Now, if the minimum stay time is later than the current time, we force the person to get on a ride.

--- assignment/Park.java
+++ assignment/Park.java
@@ -120,7 +120,7 @@ public class Park {
     			else {
     				return mostPopularShortestWaitTime;
     			}
-    			if(p.getMinStay() < Main.currentTime.getCurrentTime()) {
+    			if(p.getMinStay() >= Main.currentTime.getCurrentTime()) {
     				//assertion: attractions is sorted greatest to least by popularity
     				return mostPopularShortestWaitTime;
     			}
@@ -132,7 +132,7 @@ public class Park {
 						return attractions.get(i);
 					}
 				}
-    			if(p.getMinStay() < Main.currentTime.getCurrentTime()) {
+    			if(p.getMinStay() >= Main.currentTime.getCurrentTime()) {
     				//person hasn't stayed late enough, they suck it up and go on the ride they most desire
     				return attractions.get(0);
     			}
@@ -140,7 +140,7 @@ public class Park {
     		}
     		else if(p.isWantsMostRides()) {
     			if(this.getMinWaitTime(attractions).getWaitTime() > p.getBalkPoint()) {
-    				if(p.getMinStay() < Main.currentTime.getCurrentTime()){
+    				if(p.getMinStay() >= Main.currentTime.getCurrentTime()){
     					return this.getMinWaitTime(attractions);
     				}
     				return null;
