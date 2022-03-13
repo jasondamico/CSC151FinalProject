ec5d777eab94af6c36017ca64592f76f80563ab6
Equipy <57501201+Equipy@users.noreply.github.com>
2022-03-12 21:15:12 -0500

    is done?

--- assignment/Person.java
+++ assignment/Person.java
@@ -25,7 +25,10 @@ public class Person {
     private final int NO_BALKPOINT = 10000;
     private final int MIN_STAY = 60;
 
-    public Person() 
+    /**
+     * empty constructor
+     */
+    public Person()
     {
         this.setId(0);
         this.setMinStay(0);
@@ -43,6 +46,11 @@ public class Person {
         this.balkPoint = 0;
     }
 
+    /**
+     * constructor
+     * randomizes everything but id
+     * @param id int person id
+     */
     public Person(int id) {
         this.id = id;
 
@@ -59,7 +67,7 @@ public class Person {
             // Min stay time is at least 60 minutes after opening, could be maximum of closing time
             int minStayTime = (int) (hoursLeft * Math.random()) + this.arrivalTime  + MIN_STAY;
             if(minStay > Simulations.parkHours) {
-            	minStay = Simulations.parkHours;
+                minStay = Simulations.parkHours;
             }
             this.setMinStay(minStayTime);
 
@@ -75,6 +83,15 @@ public class Person {
         this.waitTimes = new Stack<>();
     }
 
+    /**
+     * constructor
+     * @param id person's number id
+     * @param minStay minimum time they will stay for (in minutes)
+     * @param maxStay maximum time they will stay for (in minutes)
+     * @param balkPoint the longest time they'll wait for (in minutes)
+     * @param wantsPopularRides True/False if the person wants the most popular rides
+     * @param wantsMostRides True/False if the person wants the most rides
+     */
     public Person(int id, int minStay, int maxStay, int balkPoint, boolean wantsPopularRides, boolean wantsMostRides) {
         this.id = id;
         this.minStay = minStay;
@@ -127,6 +144,10 @@ public class Person {
         this.balkPoint = balkPoint;
     }
 
+    /**
+     * isWantsPopularRides
+     * @return boolean if they want the most popular rides (True/False)
+     */
     public boolean isWantsPopularRides() {
         return wantsPopularRides;
     }
@@ -135,6 +156,10 @@ public class Person {
         this.wantsPopularRides = wantsPopularRides;
     }
 
+    /**
+     * isWantsMostRides
+     * @return boolean if they want the most rides/disregard populartity, think about wait times (True/False)
+     */
     public boolean isWantsMostRides() {
         return wantsMostRides;
     }
@@ -143,18 +168,39 @@ public class Person {
         this.wantsMostRides = wantsMostRides;
     }
 
+    /**
+     * addRideVisited
+     * adds the ride the person went on to their list of rides they've visted
+     * @param attraction Attraction the person went on
+     */
     public void addRideVisited(Attraction attraction) {
         this.ridesVisited.add(attraction);
     }
 
+    /**
+     * addFastPass
+     * give the person a fastpass
+     * @param fp FastPass to give to the person
+     */
     public void addFastPass(FastPass fp) {
         this.fastPasses.add(fp);
     }
 
-    public void addWaitTime(WaitTime waitTime) {
+    /**
+     * addWaitTime
+     * adds new WaitTime to stack of all the waits the person as been through
+     * @param waitTime the WaitTime they just went through
+     */
+    public void addWaitTime(WaitTime waitTime) 
+    {
         this.waitTimes.add(waitTime);
     }
 
+    /**
+     * peekLastWaitTime
+     * checks the most recent WaitTime
+     * @return most recent WaitTime
+     */
     public WaitTime peekLastWaitTime() {
         try {
             return this.waitTimes.peek();
@@ -163,6 +209,11 @@ public class Person {
         }
     }
 
+    /**
+     * popLastWaitTime
+     * removes the most recent WaitTime from the person's list of waits
+     * @return the most recent WaitTime
+     */
     public WaitTime popLastWaitTime() {
         try {
             return this.waitTimes.pop();
@@ -171,9 +222,14 @@ public class Person {
         }
     }
 
+    /**
+     * removeFastPass
+     * takes away a fastpass after it is used
+     * @return FastPass to be removed
+     */
     public FastPass removeFastPass() {
         if (this.fastPasses.size() > 0) {
-        	FastPass toBeUsed = fastPasses.get(0);
+            FastPass toBeUsed = fastPasses.get(0);
             this.fastPasses.remove(fastPasses.get(0));
             return toBeUsed;
         } else {
@@ -181,6 +237,10 @@ public class Person {
         }
     }
 
+    /**
+     * numFastPasses
+     * @return int of fastpasses a person has
+     */
     public int numFastPasses() {
         return this.fastPasses.size();
     }
@@ -212,6 +272,10 @@ public class Person {
         return toReturn;
     }
 
+    /**
+     * hasFastPass
+     * @return boolean (true/false) if a person has fastpasses at all
+     */
     public boolean hasFastPass() {
         if(numFastPasses() > 0) {
             return true;
@@ -220,17 +284,62 @@ public class Person {
     }
     
     public boolean getIsPremium() {
-    	return this.isPremium;
-    }
-    
-    public void setIsPremium(boolean isPremium) {
-    	this.isPremium = isPremium;
+        return this.isPremium;
     }
 
-    public ArrayList<Attraction> getRidesRidden(){
-    	return new ArrayList<Attraction>(this.ridesVisited);
+    public void setIsPremium(boolean isPremium) {
+        this.isPremium = isPremium;
     }
-    
+
+    /**
+     * getRidesRidden
+     * @return ArrayList of rides the person rode
+     */
+    public ArrayList<Attraction> getRidesRidden(){
+        return new ArrayList<Attraction>(this.ridesVisited);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (id != person.id) return false;
+        if (minStay != person.minStay) return false;
+        if (maxStay != person.maxStay) return false;
+        if (wantsPopularRides != person.wantsPopularRides) return false;
+        if (wantsMostRides != person.wantsMostRides) return false;
+        if (arrivalTime != person.arrivalTime) return false;
+        if (balkPoint != person.balkPoint) return false;
+        if (isPremium != person.isPremium) return false;
+        if (NO_BALKPOINT != person.NO_BALKPOINT) return false;
+        if (MIN_STAY != person.MIN_STAY) return false;
+        if (ridesVisited != null ? !ridesVisited.equals(person.ridesVisited) : person.ridesVisited != null)
+            return false;
+        if (fastPasses != null ? !fastPasses.equals(person.fastPasses) : person.fastPasses != null) return false;
+        return waitTimes != null ? waitTimes.equals(person.waitTimes) : person.waitTimes == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + minStay;
+        result = 31 * result + maxStay;
+        result = 31 * result + (wantsPopularRides ? 1 : 0);
+        result = 31 * result + (wantsMostRides ? 1 : 0);
+        result = 31 * result + arrivalTime;
+        result = 31 * result + (ridesVisited != null ? ridesVisited.hashCode() : 0);
+        result = 31 * result + (fastPasses != null ? fastPasses.hashCode() : 0);
+        result = 31 * result + balkPoint;
+        result = 31 * result + (waitTimes != null ? waitTimes.hashCode() : 0);
+        result = 31 * result + (isPremium ? 1 : 0);
+        result = 31 * result + NO_BALKPOINT;
+        result = 31 * result + MIN_STAY;
+        return result;
+    }
+
     public static void main(String[] args) {
         for (int i = 0; i < 20; i++) {
             System.out.println(new Person(i));
