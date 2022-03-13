69b7b1527bebea289e84c07e8beacbe3fafe43ee
Equipy <57501201+Equipy@users.noreply.github.com>
2022-03-12 21:19:44 -0500

    has description

--- assignment/Main.java
+++ assignment/Main.java
@@ -1,9 +1,14 @@
 package assignment;
-
-public class Main {
-    public static void main(String[] args) {
+/*
+The Main Class is meant to make running the simulations more compact. 
+Having this class allows us to pick which simulation to run.
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
         Simulations.noFastPassSimulation();
         //Simulations.FastPassSimulation();
-        
+
     }
 }
