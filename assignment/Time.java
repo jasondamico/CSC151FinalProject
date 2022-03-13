bd5a70dcbf9a8412148be22c8e112dd52f23e558
Equipy <57501201+Equipy@users.noreply.github.com>
2022-03-12 20:18:49 -0500

    javadocs, equals, hashcode, main, tostring

--- assignment/Time.java
+++ assignment/Time.java
@@ -1,22 +1,64 @@
-
 package assignment;
 
 public class Time
 {
     private int currentTime;
 
+    /**
+     * constructor
+     * the time starts at 0
+     */
     public Time()
     {
         this.currentTime = 0;
     }
 
+    /**
+     * getCurrentTime
+     * shows the current time (in minutes)
+     * @return
+     */
     public int getCurrentTime() //person & attraction use this
     {
         return currentTime;
     }
 
+    /**
+     * setCurrentTime
+     * changes the current time
+     * @param currentTime the time passed in minutes
+     */
     public void setCurrentTime(int currentTime) //main uses this
     {
         this.currentTime = currentTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Time time = (Time) o;
+
+        return currentTime == time.currentTime;
+    }
+
+    @Override
+    public int hashCode() {
+        return currentTime;
+    }
+
+    @Override
+    public String toString() {
+        return "" + currentTime;
+    }
+
+    public static void main(String[] args)
+    {
+        Time minutes = new Time();
+        minutes.setCurrentTime(10);
+
+        System.out.println("how many minutes have passed\n" + minutes);
+
     }
 }
