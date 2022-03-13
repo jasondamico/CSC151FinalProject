c8a42c858c4d537863598fb20008108438665a8e
PogAlGhul <101218712+PogAlGhul@users.noreply.github.com>
2022-03-13 18:32:11 -0400

    Update ParkTest.java

--- test/ParkTest.java
+++ test/ParkTest.java
@@ -110,6 +110,24 @@ public class ParkTest {
 
     @Test
     public void pickAttraction() {
-        
+        Person p = new Person();
+        p.setWantsPopularRides(true);
+        Attraction att1 = new Attraction("The Stack Cyclone", 1, 5, 10);
+        Attraction att2 = new Attraction("The Queue Coaster", 5, 8, 5);
+        ArrayList<Attraction> listOfRides = new ArrayList<>();
+        listOfRides.add(att1);
+        listOfRides.add(att2);
+        Park codeLand = new Park("codeLand", listOfRides);
+        assertEquals(codeLand.pickAttraction(p, listOfRides), listOfRides.get(1));
+    }
+    
+    @Test
+    public void pickAttraction2() {
+        Person p = new Person();
+        Attraction att1 = new Attraction("The Stack Cyclone", 1, 5, 10);
+        ArrayList<Attraction> listOfRides = new ArrayList<>();
+        listOfRides.add(att1);
+        Park codeLand = new Park("codeLand", listOfRides);
+        assertEquals(codeLand.pickAttraction(p, listOfRides), listOfRides.get(0));
     }
 }
