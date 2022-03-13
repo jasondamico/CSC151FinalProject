85bf8b8264eb16cafb2d0a76062f7daf99f13b25
PogAlGhul <101218712+PogAlGhul@users.noreply.github.com>
2022-03-13 18:22:32 -0400

    Update FastPass.java

--- assignment/FastPass.java
+++ assignment/FastPass.java
@@ -3,40 +3,33 @@ package assignment;
 import java.util.Stack;
 
 /*
-The FastPass class creates and object FastPass which is simply an object that allows a person to skip to the front of a line,
-sometimes a fastpass can only be used on a specific ride.
+The FastPass class creates and object FastPass which is simply an object that allows a person to join the FastPass queue,
+sometimes a FastPass can only be used on a specific ride.
  */
-public class FastPass
-{
-    private Attraction passAttraction;
-
+public class FastPass {
+    private String attractionName;
+    
     /**
      * empty constructor
-     * make a FastPass object (a piece of paper, effectively)
+     * make a FastPass object (a piece of paper, effectively) that works at any Attraction
      */
-    public FastPass()
-    {
-
+    public FastPass() {
+    	this.attractionName = "UNIVERSAL";
     }
 
     /**
      * constructor
      * makes an attraction specific FastPass
-     * @param passAttraction the attraction the FastPass is attached to
+     * @param attractionName the attraction the FastPass is attached to
      */
-    public FastPass(Attraction passAttraction)
-    {
-        this.passAttraction = passAttraction;
+    public FastPass(String attractionName) {
+        this.attractionName = attractionName;
     }
-
-    /**
-     * Returns the attraction the fast pass is for. 
-     * @return A reference to the attraction the fast pass is for.
-     */
-    public Attraction getAttraction() {
-        return this.passAttraction;
+    
+    public String getAttractionName() {
+    	return attractionName;
     }
-
+    
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -44,20 +37,20 @@ public class FastPass
 
         FastPass fastPass = (FastPass) o;
 
-        return passAttraction != null ? passAttraction.equals(fastPass.passAttraction) : fastPass.passAttraction == null;
+        return attractionName != null ? attractionName.equals(fastPass.attractionName) : fastPass.attractionName == null;
     }
 
     @Override
     public int hashCode() {
-        return passAttraction != null ? passAttraction.hashCode() : 0;
+        return attractionName != null ? attractionName.hashCode() : 0;
     }
 
     @Override
     public String toString()
     {
-        if (passAttraction != null)
+        if (attractionName != null)
         {
-            return "FastPass for " + passAttraction.getName();
+            return "FastPass for " + attractionName;
         }
         else
         {
@@ -70,7 +63,7 @@ public class FastPass
         Stack passes = new Stack();
         Attraction helloWorld = new Attraction("helloWorld", 5, 10, 5);
         FastPass pass1 = new FastPass();
-        FastPass pass2 = new FastPass(helloWorld);
+        FastPass pass2 = new FastPass(helloWorld.getName());
         FastPass pass3 = new FastPass();
 
         passes.add(pass1);
