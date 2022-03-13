0fafe1d7fb34bacd7ce6888b203915e5ca6d0acf
PogAlGhul <101218712+PogAlGhul@users.noreply.github.com>
2022-03-13 18:24:03 -0400

    Update Park.java

--- assignment/Park.java
+++ assignment/Park.java
@@ -2,12 +2,14 @@ package assignment;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 
 /*
 The Park Class creates the Park our customers are visting! Every park should have a list of Attractions, and a name.
 Everything that happens within a park.
  */
 public class Park {
+	
     private ArrayList<Attraction> attractions;
     private ArrayList<Person> doneForDay;
     private String name;
@@ -29,10 +31,9 @@ public class Park {
     public Park(String name) {
         attractions = new ArrayList<Attraction>();
         this.setName(name);
-
         this.doneForDay = new ArrayList<Person>();
     }
-
+    
     /**
      * constructor
      * @param name String name of the park
@@ -40,17 +41,29 @@ public class Park {
      */
     public Park(String name, ArrayList<Attraction> attractions){
         this.setName(name);
-        this.setAttractions(attractions);
+        this.attractions = new ArrayList<Attraction>();
+
+        for(int i = 0; i < attractions.size(); i++) {
+        	this.attractions.add(attractions.get(i));
+        }
+
+        // Sort in descending order (by popularity)
+        Collections.sort(this.attractions);
+        Collections.reverse(this.attractions);
 
         this.doneForDay = new ArrayList<Person>();
     }
-
+    
     /**
      * getAttractions
      * @return ArrayList of the attractions in the park
      */
     public ArrayList<Attraction> getAttractions(){
-        return new ArrayList<>(this.attractions);
+    	ArrayList<Attraction> toReturn = new ArrayList<Attraction>();
+    	for(int i = 0; i < this.attractions.size(); i++) {
+    		toReturn.add(this.attractions.get(i));
+    	}
+    	return toReturn;
     }
 
     /**
@@ -64,7 +77,7 @@ public class Park {
         Collections.sort(this.attractions);
         Collections.reverse(this.attractions);
     }
-
+    
     public String getName() {
         return name;
     }
@@ -72,118 +85,119 @@ public class Park {
     public void setName(String name) {
         this.name = name;
     }
-
+    
     /**
      * getMinWaitTime
      * @param attractions ArrayList of attractions
      * @return Attraction with the lowest wait time
      */
-    public static Attraction getMinWaitTime(ArrayList<Attraction> attractions) {
-        if(attractions.isEmpty()) {
-            return null;
-        }
-        Attraction toReturn = attractions.get(0);
-        for(int i = 0; i < attractions.size(); i ++) {
-            if(attractions.get(i).getWaitTime() < toReturn.getWaitTime()) {
-                toReturn = attractions.get(i);
-            }
-        }
-        return toReturn;
+    public Attraction getMinWaitTime(ArrayList<Attraction> attractions) {
+    	if(attractions.isEmpty()) {
+    		return null;
+    	}
+    	Attraction toReturn = attractions.get(0);
+    	for(int i = 0; i < attractions.size(); i ++) {
+    		if(attractions.get(i).getWaitTime() < toReturn.getWaitTime()) {
+    			toReturn = attractions.get(i);
+    		}
+    	}
+    	return toReturn;
     }
-
+    
     /**
      * getMaxWaitTime
      * @param attractions list of all the attractions
      * @return Attraction with the highest wait time
      */
-    public static Attraction getMaxWaitTime(ArrayList<Attraction> attractions) {
-        if(attractions.isEmpty()) {
-            return null;
-        }
-        Attraction toReturn = attractions.get(0);
-        for(int i = 0; i < attractions.size(); i ++) {
-            if(attractions.get(i).getWaitTime() > toReturn.getWaitTime()) {
-                toReturn = attractions.get(i);
-            }
-        }
-        return toReturn;
+    public Attraction getMaxWaitTime(ArrayList<Attraction> attractions) {
+    	if(attractions.isEmpty()) {
+    		return null;
+    	}
+    	Attraction toReturn = attractions.get(0);
+    	for(int i = 0; i < attractions.size(); i ++) {
+    		if(attractions.get(i).getWaitTime() > toReturn.getWaitTime()) {
+    			toReturn = attractions.get(i);
+    		}
+    	}
+    	return toReturn;
     }
-
+    
     /**
      * getThreeMostPopular
      * @return ArrayList of the 3 most popular attractions
      */
     private ArrayList<Attraction> getThreeMostPopular(){
-        ArrayList<Attraction> toReturn = new ArrayList<Attraction>();
-        for(int i = 0; i < 3; i++) {
-            toReturn.add(this.attractions.get(i));
-        }
-        return toReturn;
+    	ArrayList<Attraction> toReturn = new ArrayList<Attraction>();
+    	for(int i = 0; i < 3; i++) {
+    		toReturn.add(this.attractions.get(i));
+    	}
+    	return toReturn;
     }
-
+    
     /**
      * pickAttraction
-     * @param p
-     * @param attractions
+     * @param p 
+     * @param attractions 
      * @return Attraction to queue person p into, or null for the person to leave the park
      */
     public Attraction pickAttraction(Person p, ArrayList<Attraction> attractions) {
-        if(Simulations.currentTime.getCurrentTime() >= Simulations.parkHours) {
-            return null;
-        }
-        if(p.hasFastPass()) {
-            if(p.isWantsPopularRides()) {
-                //assertion: attractions is sorted greatest to least by popularity
-                return attractions.get(0);
-            }
-            if(p.isWantsMostRides()) {
-                return getMaxWaitTime(attractions);
-            }
-        }
-        else {
-            if(p.isWantsPopularRides() && p.isWantsMostRides()) {
-                Attraction mostPopularShortestWaitTime = getMinWaitTime(this.getThreeMostPopular());
-                if(mostPopularShortestWaitTime.getWaitTime() > p.getBalkPoint()) {
-                    for(int i = 0; i < attractions.size(); i++) {
-                        if(attractions.get(i).getWaitTime() < p.getBalkPoint()) {
-                            return attractions.get(i);
-                        }
-                    }
-                }
-                else {
-                    return mostPopularShortestWaitTime;
-                }
-                if(p.getMinStay() > Simulations.currentTime.getCurrentTime()) {
-                    //assertion: attractions is sorted greatest to least by popularity
-                    return mostPopularShortestWaitTime;
-                }
-                return null;
-            }
-            else if(p.isWantsPopularRides()) {
-                for(int i = 0; i < attractions.size(); i++) {
-                    if(attractions.get(i).getWaitTime() < p.getBalkPoint()) {
-                        return attractions.get(i);
-                    }
-                }
-                if(p.getMinStay() > Simulations.currentTime.getCurrentTime()) {
-                    //person hasn't stayed late enough, they suck it up and go on the ride they most desire
-                    return attractions.get(0);
-                }
-                return null;
-            }
-            else if(p.isWantsMostRides()) {
-                if(getMinWaitTime(attractions).getWaitTime() > p.getBalkPoint()) {
-                    if(p.getMinStay() > Simulations.currentTime.getCurrentTime()){
-                        return getMinWaitTime(attractions);
-                    }
-                    return null;
-                }
-                else {
-                    return getMinWaitTime(attractions);
-                }
-            }
-        }
-        return attractions.get((int) Math.random() * attractions.size());
+    	if(Simulations.currentTime.getCurrentTime() >= Simulations.parkHours) {
+    		return null;
+    	}
+    	if(p.hasFastPass()) {
+    		if(p.isWantsPopularRides()) {
+    			//assertion: attractions is sorted greatest to least by popularity
+    			return this.getThreeMostPopular().get((int)Math.random()*3);
+    		}
+    		if(p.isWantsMostRides()) {
+    			return this.getMinWaitTime(attractions);
+    		}
+    		return attractions.get((int) Math.random() * attractions.size());
+    	}
+    	else {
+    		if(p.isWantsPopularRides() && p.isWantsMostRides()) {
+    			Attraction mostPopularShortestWaitTime = this.getMinWaitTime(this.getThreeMostPopular());
+    			if(mostPopularShortestWaitTime.getWaitTime() > p.getBalkPoint()) {
+    				for(int i = 0; i < attractions.size(); i++) {
+    					if(attractions.get(i).getWaitTime() < p.getBalkPoint()) {
+    						return attractions.get(i);
+    					}
+    				}
+    			}
+    			else {
+    				return mostPopularShortestWaitTime;
+    			}
+    			if(p.getMinStay() > Simulations.currentTime.getCurrentTime()) {
+    				//assertion: attractions is sorted greatest to least by popularity
+    				return mostPopularShortestWaitTime;
+    			}
+    			return null;
+    		}
+    		else if(p.isWantsPopularRides()) {
+    			for(int i = 0; i < attractions.size(); i++) {
+					if(attractions.get(i).getWaitTime() < p.getBalkPoint()) {
+						return attractions.get(i);
+					}
+				}
+    			if(p.getMinStay() > Simulations.currentTime.getCurrentTime()) {
+    				//person hasn't stayed late enough, they suck it up and go on the ride they most desire
+    				return attractions.get(0);
+    			}
+    			return null;
+    		}
+    		else if(p.isWantsMostRides()) {
+    			if(this.getMinWaitTime(attractions).getWaitTime() > p.getBalkPoint()) {
+    				if(p.getMinStay() > Simulations.currentTime.getCurrentTime()){
+    					return this.getMinWaitTime(attractions);
+    				}
+    				return null;
+    			}
+    			else {
+    				return this.getMinWaitTime(attractions);
+    			}
+    		}
+    	}
+		return attractions.get((int) Math.random() * attractions.size());
 //    	return null;
     }
 
@@ -191,17 +205,17 @@ public class Park {
      * getDoneForDay
      * @return ArrayList of all the people who are done riding rides for the day
      */
-    public ArrayList<Person> getDoneForDay() {
-        return this.doneForDay;
-    }
+	public ArrayList<Person> getDoneForDay() {
+		return this.doneForDay;
+	}
 
     /**
      * addToDoneForDay
      * @param p Person to add to the doneForDay ArrayList
      */
-    public void addToDoneForDay(Person p) {
-        this.doneForDay.add(p);
-    }
+	public void addToDoneForDay(Person p) {
+		this.doneForDay.add(p);
+	}
 
     public String toString() {
         String toReturn = "Welcome to " + this.getName() + "! TIME: " + Simulations.currentTime.getCurrentTime() + "\nOur attractions are as follows:\n\n";
@@ -245,4 +259,5 @@ public class Park {
         System.out.println(p);
     }
 
+    
 }
