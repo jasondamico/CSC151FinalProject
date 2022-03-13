d842793631b7fa4c4784c29e8a77ffeee3d8f7a8
PogAlGhul <101218712+PogAlGhul@users.noreply.github.com>
2022-03-12 19:31:04 -0500

    Update Simulations.java

--- assignment/Simulations.java
+++ assignment/Simulations.java
@@ -8,7 +8,7 @@ import java.util.Collections;
 public class Simulations
 {
     public static Time currentTime = new Time();
-    public static int parkHours = 40; //8 hours
+    public static int parkHours = 480; //8 hours
     public final static double FAST_PASS_PERCENT = 0.25;
 
     private int population;
@@ -27,18 +27,24 @@ public class Simulations
     {
     	currentTime.setCurrentTime(0);
         int fastPassUsage = 0; //for finding average fastpass usage
-        int population = 20;
+        int population = 300;
 
         Attraction att1 = new Attraction("The Stack Cyclone", 1, 5, 10);
         Attraction att2 = new Attraction("The Queue Coaster", 5, 8, 5);
         Attraction att3 = new Attraction("Linked List Log Ride", 3, 6, 8);
         Attraction att4 = new Attraction("The Tree Tower", 4, 16, 5);
+        Attraction att5 = new Attraction("The Node Nightmare", 2, 10, 5);
+        Attraction att6 = new Attraction("Collection Coaster", 1, 4, 5);
+        Attraction att7 = new Attraction("The Graph Grabber", 3, 10, 10);
 
         ArrayList<Attraction> listofRides = new ArrayList<>();
         listofRides.add(att1);
         listofRides.add(att2);
         listofRides.add(att3);
         listofRides.add(att4);
+        listofRides.add(att5);
+        listofRides.add(att6);
+        listofRides.add(att7);
 
         Park codeLand = new Park("Code Land!", listofRides);
         
@@ -119,37 +125,54 @@ public class Simulations
                 if(waitTime > 0) {
                 	timeWaited += waitTime;
                 }
+                if(waitTime == 0 && p.getRidesRidden().size() != 0) {
+                	timeWaited += p.getRidesRidden().get(0).getDuration();
+                }
                 totalTimeWaited += waitTime;
             }
-            fineAdditions.add(new GarbageCollector(p.getId(), timeWaited));
+            fineAdditions.add(new GarbageCollector(p.getId(), timeWaited, p.getRidesRidden().size()));
         }
         
         System.out.println("Total time waited:" + " " + totalTimeWaited);
         System.out.println("Total number of people: " + doneForDay.size());
         System.out.println("Average time waited: " + (totalTimeWaited / doneForDay.size()));
+        System.out.println();
         
 //        Collections.sort(fineAdditions);
 //        for(int i = 0; i < fineAdditions.size(); i++) {
 //        	System.out.println(fineAdditions.get(i).getTotalTimeWaited());
+//        	//System.out.println(customers.get(fineAdditions.get(i).getId()));
+//        	//System.out.println();
+//        }
+//        
+//        System.out.println();
+//        
+//        for(int i = 0; i < fineAdditions.size(); i++) {
+//        	System.out.println(fineAdditions.get(i).getNumberOfRidesRidden());
 //        }
     }
     
     public static void FastPassSimulation() {
     	
-    	int fastPassUsage = 0; //for finding average fastpass usage
-        int population = 20;
-    	currentTime.setCurrentTime(0);
+        int fastPassUsage = 0; //for finding average fastpass usage
+        int population = 300;
+
         Attraction att1 = new Attraction("The Stack Cyclone", 1, 5, 10);
         Attraction att2 = new Attraction("The Queue Coaster", 5, 8, 5);
         Attraction att3 = new Attraction("Linked List Log Ride", 3, 6, 8);
         Attraction att4 = new Attraction("The Tree Tower", 4, 16, 5);
+        Attraction att5 = new Attraction("The Node Nightmare", 2, 10, 5);
+        Attraction att6 = new Attraction("Collection Coaster", 1, 4, 5);
+        Attraction att7 = new Attraction("The Graph Grabber", 3, 10, 10);
 
         ArrayList<Attraction> listofRides = new ArrayList<>();
         listofRides.add(att1);
         listofRides.add(att2);
         listofRides.add(att3);
         listofRides.add(att4);
-
+        listofRides.add(att5);
+        listofRides.add(att6);
+        listofRides.add(att7);
         Park codeLand = new Park("code Land!", listofRides);
         int howManyPremiumUsers = (int) (population*FAST_PASS_PERCENT);
         // Adding initial people to park
@@ -236,18 +259,31 @@ public class Simulations
                 if(waitTime > 0) {
                 	timeWaited += waitTime;
                 }
+                if(waitTime == 0 && p.getRidesRidden().size() != 0) {
+                	timeWaited += p.getRidesRidden().get(0).getDuration();
+                }
                 totalTimeWaited += waitTime;
             }
-            fineAdditions.add(new GarbageCollector(p.getId(), timeWaited));
+            fineAdditions.add(new GarbageCollector(p.getId(), timeWaited, p.getRidesRidden().size()));
         }
         
         System.out.println("Total time waited:" + " " + totalTimeWaited);
         System.out.println("Total number of people: " + doneForDay.size());
         System.out.println("Average time waited: " + (totalTimeWaited / doneForDay.size()));
+        System.out.println();
         
-//        Collections.sort(fineAdditions);
+        Collections.sort(fineAdditions);
 //        for(int i = 0; i < fineAdditions.size(); i++) {
+//        	
 //        	System.out.println(fineAdditions.get(i).getTotalTimeWaited());
+//        	//System.out.println(customers.get(fineAdditions.get(i).getId()));
+//        	//System.out.println();
+//        }
+//        
+//        System.out.println();
+//        
+//        for(int i = 0; i < fineAdditions.size(); i++) {
+//        	System.out.println(fineAdditions.get(i).getNumberOfRidesRidden());
 //        }
     }
 }
